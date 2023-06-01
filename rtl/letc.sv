2e680d160e4292dbeb5cf7b89b1008c39282e3df
John Jekel <JZJisawesome123@gmail.com>
2023-05-31 22:13:36 -0400

    Beginning stages

---
+++ rtl/letc.sv
@@ -0,0 +1,18 @@
+/* letc.sv
+ * By: John Jekel
+ *
+ * Top module of the LETC SOC
+ *
+*/
+
+module letc(
+    input logic clk,
+    input logic rst,
+
+    //TODO what ports will be exposed?
+
+);
+
+//TODO
+
+endmodule
