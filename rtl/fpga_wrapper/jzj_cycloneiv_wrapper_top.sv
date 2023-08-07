94ec2c3205116b4944e6e2af685e5db36889fcc6
John Jekel <JZJisawesome123@gmail.com>
2023-08-06 22:44:19 -0400

    Add files for my IDE for my FPGA dev board

---
+++ rtl/fpga_wrapper/jzj_cycloneiv_wrapper_top.sv
@@ -0,0 +1,20 @@
+/*
+ * File:    jzj_cycloneiv_wrapper_top.sv
+ * Brief:   TODO
+ *
+ * Copyright (C) 2023 John Jekel and Nick Chan
+ * See the LICENSE file at the root of the project for licensing info.
+ *
+ * TODO longer description
+ *
+*/
+
+module jzj_cycloneiv_wrapper_top
+    //import letc_pkg::*;
+(
+    //TODO
+);
+
+//TODO
+
+endmodule : jzj_cycloneiv_wrapper_top
