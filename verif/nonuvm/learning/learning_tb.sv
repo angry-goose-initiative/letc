9b50fa9e1e98e8ed9e563012c5639939eb183031
John Zacarias Jekel <john@jekel.ca>
2024-02-12 21:15:23 -0500

    Setup introductory files for Juan! :)

---
+++ verif/nonuvm/learning/learning_tb.sv
@@ -0,0 +1,16 @@
+/**
+ * File    learning_tb.sv
+ * Brief   Testbench for learning (System)Verilog!
+ * 
+ * Copyright:
+ *  Copyright (C) 2024 Juan Segovia
+ *  Copyright (C) 2024 John Jekel
+ * See the LICENSE file at the root of the project for licensing info.
+ *
+*/
+
+/* ------------------------------------------------------------------------------------------------
+ * Module Definition
+ * --------------------------------------------------------------------------------------------- */
+
+//TODO the rest
