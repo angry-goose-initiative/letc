205302bae5aec26779e4faf906de2904722b5d39
Eric Jessee <67602506+ericjessee@users.noreply.github.com>
2024-03-29 13:03:31 -0400

    Implemented input muxing, more testbench cases (#5)
    * added input muxing for register inputs and alu operands
    * added a few more tests to testbench
    * added output flop stage for all input passthroughs (hopefully)
    * minor improvement to testbench makefile

--- rtl/letc/core/letc_core_stage_e1.sv
+++ rtl/letc/core/letc_core_stage_e1.sv
@@ -29,9 +29,15 @@ module letc_core_stage_e1
     letc_core_tlb_if.stage dtlb_if,
 
     //Hazard/backpressure signals
-    output logic o_stage_ready,
-    input  logic i_stage_flush,
-    input  logic i_stage_stall,
+    output logic  o_stage_ready,
+    input  logic  i_stage_flush,
+    input  logic  i_stage_stall,
+
+    //bypass signals
+    input  logic  i_bypass_rs1,
+    input  logic  i_bypass_rs2,
+    input  word_t i_bypassed_rs1_data,
+    input  word_t i_bypassed_rs2_data,
 
     //From D
     input d_to_e1_s i_d_to_e1,
@@ -40,29 +46,87 @@ module letc_core_stage_e1
     output e1_to_e2_s o_e1_to_e2
 );
 
-//Temporary stubs
-assign o_e1_to_e2.valid = 1'b0;//TODO
-
 //ALU
 letc_core_alu alu(.*);
 
 word_t[1:0] i_alu_operands;
-alu_op_e i_alu_operation;
-word_t o_alu_result;
+alu_op_e    i_alu_operation;
+word_t      o_alu_result;
+
+//rs1 and rs2 bypass muxing
+word_t rs1;
+word_t rs2;
+always_comb begin
+    rs1 = i_bypass_rs1 ? i_bypassed_rs1_data : i_d_to_e1.rs1_rdata;
+    rs2 = i_bypass_rs1 ? i_bypassed_rs2_data : i_d_to_e1.rs2_rdata;
+end
+
+//ALU connections
+//op1
+always_comb begin
+    unique case (i_d_to_e1.alu_op1_src)
+        ALU_OP1_SRC_RS1:  i_alu_operands[0] = rs1;
+        ALU_OP1_SRC_PC:   i_alu_operands[0] = i_d_to_e1.pc_word;
+        ALU_OP1_SRC_CSR:  i_alu_operands[0] = i_d_to_e1.csr_rdata;
+        ALU_OP1_SRC_ZERO: i_alu_operands[0] = 32'h0;
+    endcase
+end
+//op2
+always_comb begin
+    unique case (i_d_to_e1.alu_op2_src)
+        ALU_OP2_SRC_RS1:  i_alu_operands[1] = rs1;
+        ALU_OP2_SRC_RS2:  i_alu_operands[1] = rs2;
+        ALU_OP2_SRC_IMM:  i_alu_operands[1] = i_d_to_e1.immediate;
+        ALU_OP2_SRC_FOUR: i_alu_operands[1] = 32'h4;
+    endcase
+end
+//operation
+always_comb begin
+    i_alu_operation = i_d_to_e1.alu_op;
+end
+
+//tlb interface
+//TODO tlb if logic will provide ready signal
+//rest of stage is always ready
+assign o_stage_ready = 1'b1;
+
+//output flops
+always_ff @(posedge i_clk) begin
+    if (!i_rst_n) begin
+        o_e1_to_e2.valid <= 1'b0;
+    end else begin
+        if (i_stage_flush) begin
+            o_e1_to_e2.valid <= 1'b0;
+        end else if (!i_stage_stall) begin
+            o_e1_to_e2.valid <= i_d_to_e1.valid;
+        end
+    end
+end
 
 always_ff @(posedge i_clk) begin
-    if (i_d_to_e1.valid) begin
-        i_alu_operation <= ALU_OP_ADD;
-        i_alu_operands[1] <= i_d_to_e1.rs2_rdata;
-        i_alu_operands[0] <= i_d_to_e1.rs1_rdata;
-        o_e1_to_e2.alu_result <= o_alu_result;
-    end
-    else begin
-        i_alu_operation <= ALU_OP_DO_NOTHING; //should output 0xdeadbeef
-        i_alu_operands[0] <= 32'h0;
-        i_alu_operands[1] <= 32'h0;
+    if (!i_stage_stall) begin
+        //passthroughs
+        o_e1_to_e2.rd_src <= i_d_to_e1.rd_src;
+        o_e1_to_e2.rd_idx <= i_d_to_e1.rd_idx;
+        o_e1_to_e2.rd_we  <= i_d_to_e1.rd_we;
+
+        o_e1_to_e2.csr_op <= i_d_to_e1.csr_op;
+        o_e1_to_e2.csr_idx <= i_d_to_e1.csr_idx;
+        o_e1_to_e2.old_csr_value <= i_d_to_e1.csr_rdata;
+
+        o_e1_to_e2.memory_op <= i_d_to_e1.memory_op;
+        o_e1_to_e2.memory_signed <= i_d_to_e1.memory_signed;
+        o_e1_to_e2.memory_size <= i_d_to_e1.memory_size;
+        o_e1_to_e2.rs2_rdata <= i_d_to_e1.rs2_rdata;
+        //modified outputs
         o_e1_to_e2.alu_result <= o_alu_result;
     end
 end
+
+`ifdef SIMULATION
+
+assert property (@(posedge i_clk) disable iff (!i_rst_n) !(i_stage_flush && i_stage_stall));
+
+`endif //SIMULATION
 
 endmodule : letc_core_stage_e1
--- verif/nonuvm/letc/core/stage_e1/letc_core_e1_stage_tb.sv
+++ verif/nonuvm/letc/core/stage_e1/letc_core_e1_stage_tb.sv
@@ -40,6 +40,13 @@ letc_core_tlb_if dtlb_if(.i_clk(i_clk), .i_rst_n(i_rst_n));
 d_to_e1_s i_d_to_e1;
 e1_to_e2_s o_e1_to_e2;
 
+//Bypass signals
+logic     i_bypass_rs1;
+logic     i_bypass_rs2;
+word_t    i_bypassed_rs1_data;
+word_t    i_bypassed_rs2_data;
+
+
 //Hazards
 logic o_stage_ready;
 logic i_stage_flush;
@@ -74,15 +81,20 @@ default clocking cb @(posedge i_clk);
 
     //IO
     output i_d_to_e1;
-    input o_e1_to_e2;
+    input  o_e1_to_e2;
+
+    output i_bypass_rs1;
+    output i_bypass_rs2;
+    output i_bypassed_rs1_data;
+    output i_bypassed_rs2_data;
 
     //Hazards 
-    input o_stage_ready;
+    input  o_stage_ready;
     output i_stage_flush;
     output i_stage_stall;
 
     //Debug
-    input   o_debug;
+    input  o_debug;
 
     /* verilator lint_on UNUSEDSIGNAL */
 endclocking
@@ -94,13 +106,13 @@ endclocking
 //Note: due to quirks with Verilator, sadly we should try to avoid waiting for the next posedge in tasks
 
 task setup();
-
     //Set initial input states
     cb.i_stage_stall <= 1'b0;
     cb.i_stage_flush <= 1'b0;
     cb.i_d_to_e1.valid <= 1'b0;
-    cb.i_d_to_e1.rs1_rdata <= 32'h5;
-    cb.i_d_to_e1.rs2_rdata <= 32'h5;
+    cb.i_d_to_e1.alu_op <= ALU_OP_DO_NOTHING;
+    cb.i_bypass_rs1 <= 1'b0;
+    cb.i_bypass_rs2 <= 1'b0;
 endtask
 
 /* ------------------------------------------------------------------------------------------------
@@ -108,14 +120,41 @@ endtask
  * --------------------------------------------------------------------------------------------- */
 
 initial begin
+    //assign initial values
     setup();
-
-    //Reset things
+    //reset things
     cb.i_rst_n <= 1'b0;
+    //let's add 5 and 5 to make a!
+    cb.i_d_to_e1.rs1_rdata <= 32'h5;
+    cb.i_d_to_e1.rs2_rdata <= 32'h5;
+    cb.i_d_to_e1.alu_op1_src <= ALU_OP1_SRC_RS1;
+    cb.i_d_to_e1.alu_op2_src <= ALU_OP2_SRC_RS2;
+    cb.i_d_to_e1.alu_op <= ALU_OP_ADD;
     ##2;
+    //take the stage out of reset
     cb.i_rst_n <= 1'b1;
     ##2;
+    //data valid will trigger the alu operation
     cb.i_d_to_e1.valid <= 1'b1;
+    ##1;
+    cb.i_d_to_e1.valid <= 1'b0;
+    //now let's try to subtract an immediate from rs1
+    cb.i_d_to_e1.alu_op <= ALU_OP_SUB;
+    cb.i_d_to_e1.alu_op2_src <= ALU_OP2_SRC_IMM;
+    cb.i_d_to_e1.rs1_rdata <= 32'hf;
+    cb.i_d_to_e1.immediate <= 32'h6;
+    ##2;
+    cb.i_d_to_e1.valid <= 1'b1;
+    ##1;
+    cb.i_d_to_e1.valid <= 1'b0;
+    //next let's try to increment the program counter
+    cb.i_d_to_e1.pc_word <= 32'h100000;
+    cb.i_d_to_e1.alu_op <= ALU_OP_ADD;
+    cb.i_d_to_e1.alu_op1_src <= ALU_OP1_SRC_PC;
+    cb.i_d_to_e1.alu_op2_src <= ALU_OP2_SRC_FOUR;
+    #2;
+    cb.i_d_to_e1.valid <= 1'b1;
+    //wait another 5 clock cycles and exit
     ##5;
     $finish;
 end
