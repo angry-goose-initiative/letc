ea8278c5651fa172fb544605df4a6374263d0c3a
John Jekel <john@jekel.ca>
2025-02-13 08:43:06 -0500

    Speed up IRVE and IRVE compilation, and make Github actions improvements

--- infra/make/irve.mk
+++ infra/make/irve.mk
@@ -2,5 +2,5 @@
 irve:
 	rm -rf ${REPO_ROOT}/build/irve
 	mkdir -p ${REPO_ROOT}/build/irve
-	cd ${REPO_ROOT}/build/irve && cmake ${REPO_ROOT}/verif/reference/irve
-	${MAKE} -C ${REPO_ROOT}/build/irve
+	cd ${REPO_ROOT}/build/irve && cmake -DCMAKE_BUILD_TYPE=Release ${REPO_ROOT}/verif/reference/irve
+	${MAKE} -C ${REPO_ROOT}/build/irve irve
