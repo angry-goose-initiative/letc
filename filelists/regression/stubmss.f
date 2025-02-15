2b84eba4a314e4a74e34cf4a95abe9ad22a811b7
John Jekel <john@jekel.ca>
2025-02-15 08:14:43 -0500

    Add lw to the regression; lh and lb are still broken

--- filelists/regression/stubmss.f
+++ filelists/regression/stubmss.f
@@ -9,10 +9,10 @@ build/software/rvsw/src/single_file/asm/jzjcoresoftware/callrettest.vhex8
 //build/software/rvsw/src/single_file/asm/jzjcoresoftware/fenceecalltest.vhex8//Not yet supported
 //build/software/rvsw/src/single_file/asm/jzjcoresoftware/fibbonaccijal.vhex8//Infinite loop
 //build/software/rvsw/src/single_file/asm/jzjcoresoftware/fibbonaccijalr.vhex8//Infinite loop
-//build/software/rvsw/src/single_file/asm/jzjcoresoftware/lbutest.vhex8//Not yet supported
-//build/software/rvsw/src/single_file/asm/jzjcoresoftware/lhtest.vhex8//Not yet supported
-//build/software/rvsw/src/single_file/asm/jzjcoresoftware/lhutest.vhex8//Not yet supported
-//build/software/rvsw/src/single_file/asm/jzjcoresoftware/loadbytetest.vhex8//Not yet supported
+//build/software/rvsw/src/single_file/asm/jzjcoresoftware/lbutest.vhex8//Infinite loop
+//build/software/rvsw/src/single_file/asm/jzjcoresoftware/lhtest.vhex8//Infinite loop
+//build/software/rvsw/src/single_file/asm/jzjcoresoftware/lhutest.vhex8//Infinite loop
+//build/software/rvsw/src/single_file/asm/jzjcoresoftware/loadbytetest.vhex8//Infinite loop
 build/software/rvsw/src/single_file/asm/jzjcoresoftware/luitest.vhex8
 //build/software/rvsw/src/single_file/asm/jzjcoresoftware/memoryreadtest.vhex8//Not yet supported
 //build/software/rvsw/src/single_file/asm/jzjcoresoftware/memorywritetest.vhex8//Not yet supported
@@ -26,6 +26,12 @@ build/software/rvsw/src/single_file/asm/jzjcoresoftware/testingfunctions.vhex8
 //build/software/rvsw/src/single_file/asm/jzjcoresoftware/uncondjumptest.vhex8//Infinite loop
 //build/software/rvsw/src/single_file/asm/jzjcoresoftware/uncondjumptest2.vhex8//Infinite loop
 //build/software/rvsw/src/single_file/asm/jzjcoresoftware/xoritoggle.vhex8//Infinite loop
+
+//letc_base_spec test programs
+//build/software/rvsw/src/single_file/asm/letc_base_spec/first_loads.vhex8//Not yet supported
+//build/software/rvsw/src/single_file/asm/letc_base_spec/lb.vhex8//Not yet supported
+build/software/rvsw/src/single_file/asm/letc_base_spec/lw.vhex8
+//build/software/rvsw/src/single_file/asm/letc_base_spec/lh.vhex8//Not yet supported
 
 //letc_pre_hazard test programs
 build/software/rvsw/src/single_file/asm/letc_pre_hazard/dependency_problem_0.vhex8
